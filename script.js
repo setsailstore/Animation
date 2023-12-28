75d2835a3bda29cab133ee52c91087021f0422c0
Vikoadrian <vikoadrian212@gmail.com>
2023-12-28 16:32:23 +0700

    newa

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ let  sections = document.querySelectorAll('section');
 window.onscroll =()=>{
     sections.forEach(sec =>{
         let top = window.scrollY;
-        let offset = sec.offsetTop ;
+<<<<<<< HEAD
+        let offset = sec.offsetTop - 290 ;
+=======
+        let offset = sec.offsetTop - 200 ;
+>>>>>>> 8bc07ad7a90951ac1774ba50cd9e009b50b6af75
         let height = sec.offsetHeight;
 
         if(top >= offset && top <offset + height){
