8bc07ad7a90951ac1774ba50cd9e009b50b6af75
setsailstore <149674371+setsailstore@users.noreply.github.com>
2023-12-28 16:23:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let  sections = document.querySelectorAll('section');
 window.onscroll =()=>{
     sections.forEach(sec =>{
         let top = window.scrollY;
-        let offset = sec.offsetTop ;
+        let offset = sec.offsetTop - 200 ;
         let height = sec.offsetHeight;
 
         if(top >= offset && top <offset + height){
